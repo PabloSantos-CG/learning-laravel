eccbd89d2efb91a4e58cc7cd98f38f6c3ed1a2d4
PabloSantos-CG <comercial-pablo@outlook.com>
2025-07-08 19:45:16 -0300

    update

--- b7mailer/app/Http/Controllers/Mails/AuthMailController.php
+++ b7mailer/app/Http/Controllers/Mails/AuthMailController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Mails;
 
 use App\Http\Controllers\Controller;
+use App\Mail\RegisterMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class AuthMailController extends Controller
 {
     public function sendRegisterMail() {
+        $mail = new RegisterMail();
         
+        Mail::to('test@gmail.com')->queue($mail);
+        return \redirect('/');
     }
 }
--- b7mailer/routes/web.php
+++ b7mailer/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Mails\AuthMailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/email', [AuthMailController::class, 'sendRegisterMail']);
